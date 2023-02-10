f37ea841604c0ec0ed8e2712e9688cf5dfc500c5
nkusikevin <nkusikvnhart@gmail.com>
2023-02-10 10:35:09 +0200

    adding small animations

--- 42-first-r3f-application/src/Experience.js
+++ 42-first-r3f-application/src/Experience.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
 
 function Experience() {
+	const mesh = useRef();
+	useFrame(() => {
+		mesh.current.rotation.x = mesh.current.rotation.y += 0.01;
+	});
 	return (
-		<mesh scale={2} position={[2, 3, 3]}>
+		<mesh ref={mesh} scale={2}>
 			<sphereGeometry args={[1.5, 32, 32]} />
 			<meshBasicMaterial color='mediumpurple' wireframe />
 		</mesh>
