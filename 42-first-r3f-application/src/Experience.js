6ed99f30876f11014bf37bf159ecf9ff335f17a9
nkusikevin <nkusikvnhart@gmail.com>
2023-02-10 11:25:51 +0200

    adding lights and orbitcontrols

--- 42-first-r3f-application/src/Experience.js
+++ 42-first-r3f-application/src/Experience.js
@@ -1,24 +1,32 @@
 import React from "react";
-import { useFrame } from "@react-three/fiber";
+import { useFrame, extend, useThree } from "@react-three/fiber";
 import { useRef } from "react";
-import { Group } from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
+extend({ OrbitControls });
 function Experience() {
+	const { camera, gl } = useThree();
 	const mesh = useRef();
 	useFrame((state, delta) => {
 		mesh.current.rotation.x = mesh.current.rotation.y += delta;
 	});
+
 	return (
-		<group ref={mesh}>
-			<mesh scale={2}>
-				<sphereGeometry args={[1.5, 32, 32]} />
-				<meshBasicMaterial color='mediumpurple' wireframe />
-			</mesh>
-			<mesh>
-				<torusKnotGeometry />
-				<meshNormalMaterial />
-			</mesh>
-		</group>
+		<>
+			<orbitControls args={[camera, gl.domElement]} />
+			<directionalLight position={[0, 0, 10]} intensity={1.5} />
+			<ambientLight intensity={0.5} />
+			<group ref={mesh}>
+				<mesh scale={2}>
+					<sphereGeometry args={[1.5, 32, 32]} />
+					<meshStandardMaterial color='mediumpurple' wireframe />
+				</mesh>
+				<mesh>
+					<torusKnotGeometry />
+					<meshStandardMaterial color='green' />
+				</mesh>
+			</group>
+		</>
 	);
 }
 
