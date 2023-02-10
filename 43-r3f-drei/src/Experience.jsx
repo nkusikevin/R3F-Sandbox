f30dad30357edb49d7dd0a6c55d022124070fb3d
nkusikevin <nkusikvnhart@gmail.com>
2023-02-10 20:13:47 +0200

    adding transform controls and orbitcontrols

--- 43-r3f-drei/src/Experience.jsx
+++ 43-r3f-drei/src/Experience.jsx
@@ -1,33 +1,33 @@
-import { useThree, extend } from '@react-three/fiber'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { useThree, extend } from "@react-three/fiber";
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { OrbitControls, TransformControls } from "@react-three/drei";
 
-extend({ OrbitControls })
+// extend({ OrbitControls })
 
-export default function Experience()
-{
-    const { camera, gl } = useThree()
+export default function Experience() {
+	const { camera, gl } = useThree();
 
-    return <>
+	return (
+		<>
+			<OrbitControls args={[camera, gl.domElement]} />
 
-        <orbitControls args={ [ camera, gl.domElement ] } />
+			<directionalLight position={[1, 2, 3]} intensity={1.5} />
+			<ambientLight intensity={0.5} />
 
-        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-        <ambientLight intensity={ 0.5 } />
+			<mesh position-x={-2}>
+				<sphereGeometry />
+				<meshStandardMaterial color='orange' />
+			</mesh>
 
-        <mesh position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
+			<mesh position-x={2} scale={1.5}>
+				<boxGeometry />
+				<meshStandardMaterial color='mediumpurple' />
+			</mesh>
 
-        <mesh position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
-
-        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
-
-    </>
+			<mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+				<planeGeometry />
+				<meshStandardMaterial color='greenyellow' />
+			</mesh>
+		</>
+	);
 }
