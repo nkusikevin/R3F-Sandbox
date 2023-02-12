4d4c964597d1c17c56594be3289c45b15b728217
nkusikevin <nkusikvnhart@gmail.com>
2023-02-12 21:10:30 +0200

    adding debug ui

--- 44-debug-a-r3f-application/src/Experience.jsx
+++ 44-debug-a-r3f-application/src/Experience.jsx
@@ -1,28 +1,36 @@
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls } from "@react-three/drei";
+import { useControls } from "leva";
 
-export default function Experience()
-{
-    return <>
+export default function Experience() {
+	const { position, scale, color } = useControls("sphere", {
+		position: { value: { x: -2, y: 0 }, step: 0.01, joystick: "invertY" },
+		color: { value: "#ff0000" },
+		scale: { value: 1, min: 0.1, max: 2, step: 0.1 },
+		myButton: () => console.log("hello"),
+		myInput: { value: "hello", onChange: (v) => console.log(v) },
+		mySelect: { value: "b", options: { a: "A", b: "B", c: "C" } },
+	});
+	return (
+		<>
+			<OrbitControls makeDefault />
 
-        <OrbitControls makeDefault />
+			<directionalLight position={[1, 2, 3]} intensity={1.5} />
+			<ambientLight intensity={0.5} />
 
-        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-        <ambientLight intensity={ 0.5 } />
+			<mesh position={[position.x, position.y, 0]} scale={scale}>
+				<sphereGeometry />
+				<meshStandardMaterial color={color} />
+			</mesh>
 
-        <mesh position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
+			<mesh position-x={2} scale={1.5}>
+				<boxGeometry />
+				<meshStandardMaterial color='mediumpurple' />
+			</mesh>
 
-        <mesh position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
-
-        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
-
-    </>
+			<mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+				<planeGeometry />
+				<meshStandardMaterial color='greenyellow' />
+			</mesh>
+		</>
+	);
 }
