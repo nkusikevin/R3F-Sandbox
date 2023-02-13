3a8afbc2f86a1fdbf1e2b281b8cf7c77cc352cf8
nkusikevin <nkusikvnhart@gmail.com>
2023-02-13 19:22:47 +0200

    adding light helpers inr3f

--- 45-environment-and-staging-with-r3f/src/Experience.jsx
+++ 45-environment-and-staging-with-r3f/src/Experience.jsx
@@ -1,40 +1,45 @@
-import { useFrame } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
-import { useRef } from 'react'
-import { Perf } from 'r3f-perf'
+import { useFrame } from "@react-three/fiber";
+import { OrbitControls, useHelper } from "@react-three/drei";
+import { useRef } from "react";
+import { Perf } from "r3f-perf";
+import * as THREE from "three";
 
-export default function Experience()
-{
-    const cube = useRef()
-    
-    useFrame((state, delta) =>
-    {
-        cube.current.rotation.y += delta * 0.2
-    })
+export default function Experience() {
+	const cube = useRef();
+	const directionLightRef = useRef();
+	useHelper(directionLightRef, THREE.DirectionalLightHelper, 0.5, "hotpink");
 
-    return <>
+	useFrame((state, delta) => {
+		cube.current.rotation.y += delta * 0.2;
+	});
 
-        <Perf position="top-left" />
+	return (
+		<>
+			<Perf position='top-left' />
 
-        <OrbitControls makeDefault />
+			<OrbitControls makeDefault />
 
-        <directionalLight position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-        <ambientLight intensity={ 0.5 } />
+			<directionalLight
+				position={[1, 2, 3]}
+				intensity={1.5}
+				ref={directionLightRef}
+			/>
+			<ambientLight intensity={0.5} />
 
-        <mesh position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
+			<mesh position-x={-2}>
+				<sphereGeometry />
+				<meshStandardMaterial color='orange' />
+			</mesh>
 
-        <mesh ref={ cube } position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
+			<mesh ref={cube} position-x={2} scale={1.5}>
+				<boxGeometry />
+				<meshStandardMaterial color='mediumpurple' />
+			</mesh>
 
-        <mesh position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
-
-    </>
+			<mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+				<planeGeometry />
+				<meshStandardMaterial color='greenyellow' />
+			</mesh>
+		</>
+	);
 }
