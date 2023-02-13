f1b142e6c2b60da8274750b7c0a7b708525bb240
nkusikevin <nkusikvnhart@gmail.com>
2023-02-13 20:39:14 +0200

    adding soft shadow

--- 45-environment-and-staging-with-r3f/src/Experience.jsx
+++ 45-environment-and-staging-with-r3f/src/Experience.jsx
@@ -1,8 +1,20 @@
 import { useFrame } from "@react-three/fiber";
-import { OrbitControls, useHelper } from "@react-three/drei";
+import {
+	OrbitControls,
+	useHelper,
+	BakeShadows,
+	softShadows,
+} from "@react-three/drei";
 import { useRef } from "react";
 import { Perf } from "r3f-perf";
 import * as THREE from "three";
+softShadows({
+	frustum: 3.75,
+	size: 0.005,
+	near: 9.5,
+	samples: 17,
+	rings: 11,
+});
 
 export default function Experience() {
 	const cube = useRef();
@@ -15,6 +27,7 @@ export default function Experience() {
 
 	return (
 		<>
+			<BakeShadows />
 			<Perf position='top-left' />
 
 			<OrbitControls makeDefault />
@@ -23,20 +36,32 @@ export default function Experience() {
 				position={[1, 2, 3]}
 				intensity={1.5}
 				ref={directionLightRef}
+				castShadow
+				shadow-mapSize={[1024, 1024]}
+				shadow-camera-near={1}
+				shadow-camera-far={10}
+				shadow-camera-left={-5}
+				shadow-camera-right={5}
+				shadow-camera-top={5}
+				shadow-camera-bottom={-5}
 			/>
 			<ambientLight intensity={0.5} />
 
-			<mesh position-x={-2}>
+			<mesh castShadow position-x={-2}>
 				<sphereGeometry />
 				<meshStandardMaterial color='orange' />
 			</mesh>
 
-			<mesh ref={cube} position-x={2} scale={1.5}>
+			<mesh castShadow ref={cube} position-x={2} scale={1.5}>
 				<boxGeometry />
 				<meshStandardMaterial color='mediumpurple' />
 			</mesh>
 
-			<mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+			<mesh
+				receiveShadow
+				position-y={-1}
+				rotation-x={-Math.PI * 0.5}
+				scale={10}>
 				<planeGeometry />
 				<meshStandardMaterial color='greenyellow' />
 			</mesh>
--- 45-environment-and-staging-with-r3f/src/index.jsx
+++ 45-environment-and-staging-with-r3f/src/index.jsx
@@ -7,6 +7,7 @@ const root = ReactDOM.createRoot(document.querySelector("#root"));
 
 root.render(
 	<Canvas
+		shadows
 		camera={{
 			fov: 45,
 			near: 0.1,
