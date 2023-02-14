76a00b1c3290d6ae5f4595cff1ceae70045cbfb2
nkusikevin <nkusikvnhart@gmail.com>
2023-02-14 13:05:15 +0200

    adding faox animations

--- 46-load-models-with-r3f/src/Fox.jsx
+++ 46-load-models-with-r3f/src/Fox.jsx
@@ -1,19 +1,23 @@
 import React, { useEffect } from "react";
 import { useGLTF, useAnimations } from "@react-three/drei";
+import { useControls } from "leva";
 
 function Fox() {
 	const fox = useGLTF("./Fox/glTF/Fox.gltf");
 	const animations = useAnimations(fox.animations, fox.scene);
 
+	const { animationName } = useControls({
+		animationName: { options: ["Survey", "Walk", "Run"] },
+	});
+
 	useEffect(() => {
-		const action = animations.actions.Survey;
-		window.setTimeout(() => {
-			animations.actions.Run.play();
-			animations.actions.Run.crossFadeFrom(animations.actions.Survey, 1);
-		}, 2000);
-		action.play();
-	}, [animations]);
-	console.log(fox);
+		const action = animations.actions[animationName];
+		action.reset().fadeIn(0.5).play();
+		return () => {
+			action.fadeOut(0.5);
+		};
+	}, [animations, animationName]);
+
 	return (
 		<primitive
 			object={fox.scene}
