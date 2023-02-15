d837e3798b19ed5991ff5e05facb3b0364a53792
nkusikevin <nkusikvnhart@gmail.com>
2023-02-15 11:10:44 +0200

    init 3dtext<

---
+++ 47-3d-text-with-r3f-final/src/Experience.jsx
@@ -0,0 +1,79 @@
+import { useMatcapTexture, Center, Text3D, OrbitControls } from '@react-three/drei'
+import { Perf } from 'r3f-perf'
+import { useEffect, useRef, useState } from 'react'
+import { useFrame } from '@react-three/fiber'
+import * as THREE from 'three'
+
+const torusGeometry = new THREE.TorusGeometry(1, 0.6, 16, 32)
+const material = new THREE.MeshMatcapMaterial()
+
+export default function Experience()
+{
+    const donuts = useRef([])
+
+    const [ matcapTexture ] = useMatcapTexture('7B5254_E9DCC7_B19986_C8AC91', 256)
+
+    useFrame((state, delta) =>
+    {
+        for(const donut of donuts.current)
+        {
+            donut.rotation.y += delta * 0.2
+        }
+    })
+
+    useEffect(() =>
+    {
+        matcapTexture.encoding = THREE.sRGBEncoding
+        matcapTexture.needsUpdate = true
+
+        material.matcap = matcapTexture
+        material.needsUpdate = true
+    }, [])
+
+    return <>
+
+        <Perf position="top-left" />
+
+        <OrbitControls makeDefault />
+
+        <Center>
+            <Text3D
+                material={ material }
+                font="./fonts/helvetiker_regular.typeface.json"
+                size={ 0.75 }
+                height={ 0.2 }
+                curveSegments={ 12 }
+                bevelEnabled
+                bevelThickness={ 0.02 }
+                bevelSize={ 0.02 }
+                bevelOffset={ 0 }
+                bevelSegments={ 5 }
+            >
+                HELLO R3F
+            </Text3D>
+        </Center>
+
+        { [...Array(100)].map((value, index) =>
+            <mesh
+                ref={ (element) => donuts.current[index] = element }
+                key={ index }
+                geometry={ torusGeometry }
+                material={ material }
+                position={ [
+                    (Math.random() - 0.5) * 10,
+                    (Math.random() - 0.5) * 10,
+                    (Math.random() - 0.5) * 10
+                ] }
+                scale={ 0.2 + Math.random() * 0.2 }
+                rotation={ [
+                    Math.random() * Math.PI,
+                    Math.random() * Math.PI,
+                    0
+                ] }
+            >
+                <torusGeometry args={ [ 1, 0.6, 16, 32 ] } />
+            </mesh>
+        ) }
+
+    </>
+}
