49743734d85d6e1ddfcee6ec58cc0d3ae0b37425
nkusikevin <nkusikvnhart@gmail.com>
2023-02-20 18:08:13 +0200

    adding bloom effect

--- 50-post-processing-with-r3f/src/Experience.jsx
+++ 50-post-processing-with-r3f/src/Experience.jsx
@@ -35,9 +35,9 @@ export default function Experience() {
 					mipmapBlur
 					// blendFunction={BlendFunction.COLOR_DODGE}
 					// kernelSize={Bloom.KernelSize.LARGE}
-					// luminanceThreshold={0.9}
+					luminanceThreshold={0}
 					// luminanceSmoothing={0.025}
-					// intensity={0.5}
+					intensity={0.1}
 					// height={300}
 				/>
 			</EffectComposer>
@@ -51,12 +51,12 @@ export default function Experience() {
 
 			<mesh castShadow position-x={-2}>
 				<sphereGeometry />
-				<meshStandardMaterial color='orange' />
+				<meshStandardMaterial color={[5, 2, 1]} toneMapped={false} />
 			</mesh>
 
 			<mesh castShadow position-x={2} scale={1.5}>
 				<boxGeometry />
-				<meshStandardMaterial color={[1.5, 1, 4]} toneMapped={false} />
+				<meshStandardMaterial color='orange' toneMapped={false} />
 			</mesh>
 
 			<mesh
