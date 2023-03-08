fe940a78132e0637571c486c38c6a981cb7f56fd
nkusikevin <nkusikvnhart@gmail.com>
2023-03-08 11:18:18 +0200

    usign buld in colliders

--- 52-physics-with-r3f/src/Experience.jsx
+++ 52-physics-with-r3f/src/Experience.jsx
@@ -1,31 +1,40 @@
-import { OrbitControls } from '@react-three/drei'
-import { Perf } from 'r3f-perf'
+import { OrbitControls } from "@react-three/drei";
+import { Perf } from "r3f-perf";
+import { Debug, Physics, RigidBody } from "@react-three/rapier";
 
-export default function Experience()
-{
-    return <>
+export default function Experience() {
+	return (
+		<>
+			<Perf position='top-left' />
 
-        <Perf position="top-left" />
+			<OrbitControls makeDefault />
 
-        <OrbitControls makeDefault />
-
-        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 1.5 } />
-        <ambientLight intensity={ 0.5 } />
-
-        <mesh castShadow position={ [ - 2, 2, 0 ] }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
-
-        <mesh castShadow position={ [ 2, 2, 0 ] }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
-
-        <mesh receiveShadow position-y={ - 1.25 }>
-            <boxGeometry args={ [ 10, 0.5, 10 ] } />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
-
-    </>
+			<directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
+			<ambientLight intensity={0.5} />
+			<Physics>
+				<Debug />
+				<RigidBody colliders='ball'>
+					<mesh castShadow position={[0, 4, 0]}>
+						<sphereGeometry />
+						<meshStandardMaterial color='orange' />
+					</mesh>
+				</RigidBody>
+				<RigidBody colliders='trimesh'>
+					<mesh
+						castShadow
+						position={[0, 1, 0]}
+						rotation={[Math.PI * 0.5, 0, 0]}>
+						<torusGeometry args={[1, 0.5, 16, 32]} />
+						<meshStandardMaterial color='mediumpurple' />
+					</mesh>
+				</RigidBody>
+				<RigidBody type='fixed'>
+					<mesh receiveShadow position-y={-1.25}>
+						<boxGeometry args={[10, 0.5, 10]} />
+						<meshStandardMaterial color='greenyellow' />
+					</mesh>
+				</RigidBody>
+			</Physics>
+		</>
+	);
 }
