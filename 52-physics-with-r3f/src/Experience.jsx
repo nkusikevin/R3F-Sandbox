a4b61cfe6693483b9c05e975b445132c92b0a88c
nkusikevin <nkusikvnhart@gmail.com>
2023-03-09 11:22:59 +0200

    adding rotation on rigidbody

--- 52-physics-with-r3f/src/Experience.jsx
+++ 52-physics-with-r3f/src/Experience.jsx
@@ -8,18 +8,35 @@ import {
 	CuboidCollider,
 	BallCollider,
 } from "@react-three/rapier";
+import { useFrame } from "@react-three/fiber";
+import * as THREE from "three";
 
 export default function Experience() {
 	const cube = useRef();
+	const twister = useRef();
 
 	const cubeJump = () => {
-		cube.current.applyImpulse({ x: 0, y: 5, z: 0 });
+		const mass = cube.current.mass();
+		cube.current.applyImpulse({ x: 0, y: 5 * mass, z: 0 });
 		cube.current.applyTorqueImpulse({
 			x: Math.random() - 0.5,
 			y: Math.random() - 0.5,
 			z: Math.random() - 0.5,
 		});
 	};
+
+	useFrame((state) => {
+		const time = state.clock.getElapsedTime();
+		const euler = new THREE.Euler(0, time, 0);
+		const quaternion = new THREE.Quaternion();
+		quaternion.setFromEuler(euler);
+		twister.current.setNextKinematicRotation(quaternion);
+
+		const angle = time * 0.5;
+		const x = Math.cos(angle) * 2;
+		const z = Math.sin(angle) * 2;
+		twister.current.setNextKinematicTranslation({ x, y: -0.8, z });
+	});
 	return (
 		<>
 			<Perf position='top-left' />
@@ -36,16 +53,27 @@ export default function Experience() {
 						<meshStandardMaterial color='orange' />
 					</mesh>
 				</RigidBody>
-				<RigidBody colliders='cuboid' ref={cube}>
+				<RigidBody colliders={false} ref={cube} restitution={0} friction={0.7}>
 					<mesh castShadow onClick={cubeJump}>
 						<boxGeometry />
 						<meshStandardMaterial color='mediumpurple' />
 					</mesh>
+					<CuboidCollider args={[0.5, 0.5, 0.5]} mass={1} />
 				</RigidBody>
 				<RigidBody type='fixed'>
 					<mesh receiveShadow position-y={-1.25}>
 						<boxGeometry args={[10, 0.5, 10]} />
 						<meshStandardMaterial color='greenyellow' />
+					</mesh>
+				</RigidBody>
+				<RigidBody
+					position={[0, -0.8, 0]}
+					friction={0}
+					type='kinematicPosition'
+					ref={twister}>
+					<mesh castShadow scale={[0.4, 0.4, 3]}>
+						<boxGeometry />
+						<meshStandardMaterial color='red' />
 					</mesh>
 				</RigidBody>
 			</Physics>
